3940f29fdac18ea23ccaf33771e15096fe80d537
DKendle <dina.kendle@gmail.com>
2021-01-20 14:55:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function scooter(year, color, model){
+function Scooter(year, color, model){
     this.year = year;
     this.color = color;
     this.model = model
 }
 
-function driver(name, age, experience){
+function Driver(name, age, experience){
     this.name = name;
     this.age = age;
     this.experience = experience
 }
 
-function pickupLocation(address, city){
+function PickupLocation(address, city){
     this.address = address;
     this.city = city
 }
